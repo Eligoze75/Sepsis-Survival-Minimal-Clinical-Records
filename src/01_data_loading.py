88d4b69ef21fcb19ba6264485a538be20bdacfd5
eligoze <eligoze75@gmail.com>
2025-12-06 13:28:26 -0800

    Applied black formatting, added default options to click params, only importing used functions from utils and validations

--- src/01_data_loading.py
+++ src/01_data_loading.py
@@ -1,23 +1,28 @@
 import click
-import os
-from utils import *
-from validations import *
+from utils import load_ucisepsis
+from validations import check_file_format
 import sys
+import os
+
+DEFAULT_FILENAME = "s41598-020-73558-3_sepsis_survival_primary_cohort.csv"
+DEFAULT_OUTPUTNAME = os.path.join(os.pardir(__file__), "data")
+
 
 @click.command()
 @click.option(
     "--filename",
     "-n",
+    default=DEFAULT_FILENAME,
     required=True,
     help="Name of CSV file INSIDE the UCI ZIP archive to load",
 )
 @click.option(
     "--output",
     "-o",
+    default=DEFAULT_OUTPUTNAME,
     required=True,
     help="Path to directory where raw data will be written to",
 )
-
 def download_data(filename, output):
     """
     Downloads the UCI Sepsis Survival dataset ZIP,
@@ -45,14 +50,14 @@ def download_data(filename, output):
         click.echo("File format validation failed.")
         click.echo(str(e))
         sys.exit("Stopping execution due to invalid file format.")
-    
+
     # Save output
     try:
         output_dir = os.path.dirname(output)
         # To ensure the output path exists
         if output_dir:
             os.makedirs(output_dir, exist_ok=True)
-    
+
         df.to_csv(output, index=False)
         click.echo(f"Saved dataset to: {output}")
     except Exception as e:
