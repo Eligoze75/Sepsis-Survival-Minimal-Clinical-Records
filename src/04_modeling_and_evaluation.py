dcd135532ff989f186d7cfb79edc031d613642c1
eligoze <eligoze75@gmail.com>
2025-12-06 19:00:22 -0800

    Removed unused libraries and methods

--- src/04_modeling_and_evaluation.py
+++ src/04_modeling_and_evaluation.py
@@ -1,42 +1,37 @@
-# sepsis_modeling_and_evaluation.py
-# date: 2025-12-05
-import click
-import pickle
 import pandas as pd
-from scipy.stats import loguniform, uniform, randint
+from scipy.stats import loguniform
 from sklearn.compose import make_column_transformer
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import (StandardScaler, OneHotEncoder)
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.metrics import (
     roc_auc_score,
     precision_score,
     recall_score,
     f1_score,
-    log_loss,
 )
-from sklearn.model_selection import (
-    RandomizedSearchCV,
-    cross_val_score,
-    cross_validate,
-    train_test_split,
-)
+from sklearn.model_selection import RandomizedSearchCV
+import click
+import pickle
+
 
 @click.command()
 @click.option("--train-df", type=str, required=True, help="Path to cleaned TRAIN CSV")
 @click.option("--test-df", type=str, required=True, help="Path to cleaned TEST CSV")
-@click.option('--output-table', type=str, help="Path to directory where the table will be written to")
-
+@click.option(
+    "--output-table",
+    type=str,
+    help="Path to directory where the table will be written to",
+)
 def main(train_df, test_df, output_table):
-    '''Reads and splits the cleaned data, fits a sepsis prediction model, and outputs a table summarizing the classification metrics.'''
+    """Reads and splits the cleaned data, fits a sepsis prediction model, and outputs a table summarizing the classification metrics."""
 
     RANDOM_STATE = 15
-    
+
     # Read and split the data
     train_df = pd.read_csv(train_df)
     test_df = pd.read_csv(test_df)
-    
+
     X_train, y_train = (
         train_df.drop(columns=["hospital_outcome", "hospital_outcome_cat"]),
         train_df["hospital_outcome"],
@@ -57,13 +52,13 @@ def main(train_df, test_df, output_table):
         (StandardScaler(), numeric_features),
         (OneHotEncoder(drop="if_binary"), categorical_features),
     )
-    
+
     # Create Pipeline
     logistic_pipe = make_pipeline(
         lr_preprocessor,
         LogisticRegression(random_state=RANDOM_STATE),
     )
-    
+
     # Tune the model
     param_grid = {
         "logisticregression__C": loguniform(1e-4, 1e2),
@@ -78,14 +73,14 @@ def main(train_df, test_df, output_table):
         n_jobs=-1,
         random_state=RANDOM_STATE,
         return_train_score=True,
-        cv=5
+        cv=5,
     )
     lr_random_search.fit(X_train, y_train)
-    lr_best_model = lr_random_search.best_estimator_   
+    lr_best_model = lr_random_search.best_estimator_
 
-    with open("lr_best_model.pickle", 'wb') as f:
+    with open("lr_best_model.pickle", "wb") as f:
         pickle.dump(lr_best_model, f)
-    
+
     # Classification Metrics(adapted from DSCI 573 lecture 1)
     y_pred_train = lr_best_model.predict(X_train)
     y_pred_test = lr_best_model.predict(X_test)
@@ -107,19 +102,20 @@ def main(train_df, test_df, output_table):
             "F1 score": [f1_train, f1_test],
         }
     )
-       
-    with open("classification_metrics.pickle", 'wb') as f:
+
+    with open("classification_metrics.pickle", "wb") as f:
         pickle.dump(classification_metrics, f)
 
     # Outputs classification metrics table
     try:
         classification_metrics.to_csv(output_table, index=False)
-        
+
     except Exception as e:
         print("Error saving output files:", e)
         sys.exit(1)
 
     print(f"Saved classification metrics to: {output_table}")
+
 
 if __name__ == "__main__":
     main()
